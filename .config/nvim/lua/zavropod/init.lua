7c465a5ae8715d911ca60e4949b4e68a1412f6cc
miklisanton <miklis2003@mail.ru>
2025-02-01 15:16:35 +0300

    initial commit

---
+++ .config/nvim/lua/zavropod/init.lua
@@ -0,0 +1,10 @@
+-- Set tab width to 2 spaces for JavaScript files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+  callback = function()
+    print("Setting tab width to 2 spaces for JavaScript files")
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
